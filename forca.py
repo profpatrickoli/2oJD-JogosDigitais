39124b5160ea7b69b1beb4c29cc863e49af4e8ee
profpatrickoli <patrick.staroin@escola.pr.gov>
2024-03-25 12:32:40 -0300

    Alterações na forca

--- forca.py
+++ forca.py
@@ -1,38 +1,39 @@
 import random
 def jogar() :
-    sorteio_palavra_int = random.randint(1,3)
+    palavras = ("estrela", "cafe", "balacobaco")
+    palavra_secreta = random.choice(palavras)
     print("###### Bem vindo ao jogo da FORCA ######")
-    if sorteio_palavra_int == 1:
-        palavra_secreta = "estrela"
-    elif sorteio_palavra_int == 2:
-        palavra_secreta = "cafe"
-    elif sorteio_palavra_int == 3:
-        palavra_secreta = "balacobaco"
-
     letras_acertadas = []
     for letra in palavra_secreta:
         letras_acertadas.append("_")
-    #letras_acertadas = ["_", "_", "_", "_", "_", "_", "_"]
     acertou = False
     enforcou = False
+    max_erros = 6
+    erros = 0
 
-    
     while(not acertou and not enforcou):
         print(letras_acertadas)
+        print("Você tem {} tentativas!".format(max_erros - erros))
         chute = input("Digite a letra\n")
-
-        print("Você digitou", chute)
+        chute = chute.strip()
         index = 0
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()):
-                print("Letra '{}' encontrada na posição {}".format(chute,index))
-                letras_acertadas[index] = chute            
-            index = index + 1
+        if chute in palavra_secreta:
+            for letra in palavra_secreta:
+                if (chute.upper() == letra.upper()):
+                    letras_acertadas[index] = chute.upper()           
+                index = index + 1
+        else:
+            erros = erros + 1
 
         if letras_acertadas.count("_") == 0:
             acertou = True
-            print("Parabéns, você acertou a palavra!")
+            print(letras_acertadas)
+            print("Parabéns, você acertou a palavra! A palavra correta era", palavra_secreta)
         
+        if (erros == max_erros):
+            enforcou = True
+            print(letras_acertadas)
+            print("A palavra correta era", palavra_secreta)
     ## PROGRAMAÇÃO CRIADA
     print("##### FIM DO JOGO #####")
 
